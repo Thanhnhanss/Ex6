2141b49d82025caa4f23d54b9ebb5b1c7c89cfd8
Thanhnhanss <@gmail.com>
2019-10-27 22:39:23 +0700

    Queue

---
+++ Problem3-Queue/Problem3-Queue/Queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_Queue
+{
+    class Queue
+    {
+        private int [] Buffer;
+        private int top;
+        private int bot;
+        public Queue (int size)
+        {
+            Buffer = new int[size];
+            top = 0;
+            bot = 0;
+        }
+
+        public void EnQueue( int n)
+        {
+          if (top == Buffer.Length)
+          {
+               Console.WriteLine("Full");
+          }
+           Buffer[top ++] = n;
+        }
+        public int DeQueue ()
+        {
+            if ( bot >= top)
+            {
+                Console.WriteLine("Empty!!");
+            }
+            return Buffer[bot++];
+        }
+        public int Count
+        {
+            get
+            {
+                return bot - top;
+            }
+        }
+        public int Peek()
+        {
+            if ( bot >= top)
+            {
+                Console.WriteLine();
+            } 
+            return Buffer[bot];
+        }
+    }
+}
